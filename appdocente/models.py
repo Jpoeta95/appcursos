19368eac0517e61802ed3f73959bc6a5b8ae0d63
José Elar Villanueva Dávila <Jos Elar Villanueva Dvila>
2016-01-17 12:48:27 -0500

    te amo nuey

--- appdocente/models.py
+++ appdocente/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from aplicurso.models import curso
 
 # Create your models here.
 class docente(models.Model):
@@ -8,3 +9,6 @@ class docente(models.Model):
     ape_mat           = models.CharField(max_length=18)
     fecha_agregado    = models.DateTimeField(auto_now_add=True)
     fecha_actualizado = models.DateTimeField(auto_now=True)
+    docente_curso     = models.ManyToManyField(curso)
+    def __str__(self):
+       return '%s' % (self.nombre)
