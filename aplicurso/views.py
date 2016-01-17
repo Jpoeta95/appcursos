64ccb4fc9729e9804789cbb14b871830450690b9
nury estrella gamonal <nury estrella gamonal>
2016-01-17 13:00:23 -0500

    corsoso

--- aplicurso/views.py
+++ aplicurso/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import tipocurso, curso
 # Create your views here.
+
+def tipocursos(request):
+    if request.method == "POST": #esto sirve para preguntar si el método es post
+        f = tipocursoForm(request.POST) #esto sirve para rescatar los datos del formulario
+
+        if f.is_valid(): #esto sirve para validad formulario
+            tp = tipocurso()
+            tp.descripciontipocurso = request.POST["descripciontipocurso"]
+            tp.save()
+
+            tc = tipocurso.objects.all()
+            formulario = tipocursoForm()
+
+        else:
+            formulario = f
+    else:
+        tc = tipocurso.objects.all().order_by('id')[:5]
+        print(tc.query)
+        formulario = tipocursoForm()
+    return render(request, 'tipocurso.html', {'formu': formulario, 'tipocurso':tc})
+
+
+def cursillo(request):
+    cu = curso.objects.all()
+    formulario = cursoForm()
+
+    if request.method == "POST":
+        f = cursoForm(request.POST)
+        if f.is_valid():
+            f.save()
+        else:
+            formulario = f
+
+    return render(request, 'curso.html', {'formu': formulario, 'cursos':cu })
